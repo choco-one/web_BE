4604b636e471b2b8fc6b739249825f6a355d34c2
choi-jaewon <sodlapdlf13@gmail.com>
2021-05-28 18:27:36 +0900

    finished add and init delete

--- src/addComment.php
+++ src/addComment.php
@@ -1,3 +1,15 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
 <?php
-$conn = mysqli_connect("goweb.dothome.co.kr/myadmin","goweb","webproject333!");
-$db = mysqli_select_db($conn, "guestbook");
+$conn = mysqli_connect("localhost", "goweb", "webproject333!");
+$db = mysqli_select_db($conn, "goweb");
+$sql = "INSERT INTO guestbook (wname, wpass, content, w3) VALUES('$_POST[wname]', '$_POST[wpass]', '$_POST[content]', '$_POST[w3]')";
+$conn->query($sql);
+echo "<script>alert('글이 등록되었습니다.');";
+echo "location.href='index.html';</script>";
+?>
+</body>
+</html>
--- src/deleteComment.php
+++ src/deleteComment.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
 <?php
+$conn = mysqli_connect("localhost", "goweb", "webproject333!");
+$db = mysqli_select_db($conn, "goweb");
+if ($_GET['mode']!="delete") {
+    ?>
+    <form method="post" action="<?=$_SERVER[PHP_SELF]?>?id=<?=$_GET[wid]?>&mode='delete'">
+        <table border=1>
+            <tr>
+                <td>비밀번호</td>
+                <td><input type="password" name="wpass"/></td>
+                <td><input type="submit" value="확인"/></td>
+            </tr>
+        </table>
+    </form>
+    <?php
+    exit;
+}
+$sql = "SELECT wpass FROM guestbook WHERE wid='$_GET[wid]'";
+$result = $conn->query($sql);
+$row = $result->fetch_array();
+if ($row['wpass'] == $_POST['wpass']) {
+    $sql = "DELETE FROM guestbook WHERE wid='$_GET[wid]'";
+    $result = $conn->query($sql);
+    echo "<script>alert('글이 삭제되었습니다');";
+} else {
+    echo "<script>alert('비밀번호가 틀렸습니다');";
+}
+echo "location.href='index.html'</script>";
+?>
+</body>
+</html>
