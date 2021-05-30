5fe1d74635ee193f26494bb7816e221756536af8
choi-jaewon <sodlapdlf13@gmail.com>
2021-05-31 01:09:10 +0900

    renew

---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$user = 'goweb';
+$pw = 'webproject333!';
+$dbName = 'goweb';
+$conn = mysqli_connect($host, $user, $pw, $dbName);
+?>
---
+++ src/test.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$user = 'goweb';
+$pw = 'webproject333!';
+$dbName = 'goweb';
+
+$conn = mysqli_connect($host, $user, $pw, $dbName);
+
+$sql = "SELECT * FROM guestbook";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+
+$list_array = array("wname" => $row['wname'],
+    "wdate" => $row['wdate'],
+    "content" => $row['content']);
+
+$result_array = json_encode($list_array);
+
+echo $result_array;
+?>
